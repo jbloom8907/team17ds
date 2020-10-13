33caf4379aa7ccb27cf25942f4aac0610cba3d68
jbloom8907 <jbloom8907@gmail.com>
2020-10-13 14:14:04 -0400

    Commenting out column

--- app/public/js/data.app.js
+++ app/public/js/data.app.js
@@ -45,12 +45,12 @@ var app = new Vue({
 
       this.newMemberForm = this.newMemberData();
     },
-    handleDataForm( evt ) {
-      console.log("Form submitted!");
-
-      this.dataForm.member = this.activeMember;
-      console.log(this.dataForm);
-    }
+    // handleDataForm( evt ) {
+    //   console.log("Form submitted!");
+    //
+    //   this.dataForm.member = this.activeMember;
+    //   console.log(this.dataForm);
+    // }
   },
   created() {
     fetch("api/members/")
