199d6877f76d6c5386b5bc5de46dccd19640ff7e
jbloom8907 <jbloom8907@gmail.com>
2020-10-13 19:27:03 -0400

    Edit member data api

---
+++ app/public/api/updates/memberData.php
@@ -0,0 +1,43 @@
+<?php
+
+require 'common.php';
+
+// Step 0: Validate the incoming data
+// This code doesn't do that, but should ...
+// For example, if the date is empty or bad, this insert fails.
+
+// Step 1: Get a datase connection from our helper class
+$db = DbConnection::getConnection();
+
+// Step 2: Create & run the query
+// Note the use of parameterized statements to avoid injection
+$stmt = $db->prepare(
+  'UPDATE member
+  SET dob = ?, gender = ?, phonePrimary = ?, phoneSecondary = ?, email = ?, street = ?, city = ?, state = ?, zip = ?, station = ?, title = ?, active = ?, radioNum  = ?
+  WHERE fName = "Jordan" AND lName = "Bloom"'
+);
+
+$stmt->execute([
+  $_POST['dob'],
+  $_POST['gender'],
+  $_POST['phonePrimary'],
+  $_POST['phoneSecondary'],
+  $_POST['email'],
+  $_POST['street'],
+  $_POST['city'],
+  $_POST['state'],
+  $_POST['zip'],
+  $_POST['station'],
+  $_POST['title'],
+  $_POST['active'],
+  $_POST['radioNum']
+]);
+
+// If needed, get auto-generated PK from DB
+// $pk = $db->lastInsertId();  // https://www.php.net/manual/en/pdo.lastinsertid.php
+
+// Step 4: Output
+// Here, instead of giving output, I'm redirecting to the SELECT API,
+// just in case the data changed by entering it
+header('HTTP/1.1 303 See Other');
+header('Location: ../updates/');
--- app/public/js/data.app.js
+++ app/public/js/data.app.js
@@ -3,6 +3,7 @@ var app = new Vue({
   data: {
     memberList: [],
     activeMember: null,
+    dataForm: {},
     newMemberForm: {}
   },
   computed: {
@@ -15,6 +16,23 @@ var app = new Vue({
       return {
         fName: "",
         lName: "",
+        dob: "",
+        gender: "",
+        phonePrimary: "",
+        phoneSecondary: "",
+        email: "",
+        street: "",
+        city: "",
+        state: "",
+        zip: "",
+        station: "",
+        title: "",
+        active: "",
+        radioNum: ""
+      }
+    },
+    newFormData() {
+      return {
         dob: "",
         gender: "",
         phonePrimary: "",
@@ -50,28 +68,28 @@ var app = new Vue({
       console.log("Creating (POSTing)...!");
       console.log(this.newMemberForm);
     },
-    // handleDataForm( evt ) {
-    //   console.log("Edit form submitted!");
-    //
-    //   if (!this.activeMember) {
-    //     alert("ERROR: No member selected!");
-    //     return false;
-    //   }
-    //
-    //   fetch('api/updates/memberData.php', {
-    //     method:'POST',
-    //     body: JSON.stringify(this.dataForm),
-    //     headers: {
-    //       "Content-Type": "application/json; charset=utf-8"
-    //     }
-    //   })
-    //   .then( response => response.json() )
-    //   .then( json => {
-    //     console.log("Returned from member data:", json);
-    //     this.memberList.push(json[0]);
-    //     this.newDataForm = this.newMemberData();
-    //   });
-    // }
+    handleDataForm( evt ) {
+      console.log("Edit form submitted!");
+
+      if (!this.activeMember) {
+        alert("ERROR: No member selected!");
+        return false;
+      }
+
+      fetch('api/updates/memberData.php', {
+        method:'POST',
+        body: JSON.stringify(this.dataForm),
+        headers: {
+          "Content-Type": "application/json; charset=utf-8"
+        }
+      })
+      .then( response => response.json() )
+      .then( json => {
+        console.log("Returned from member data:", json);
+        this.newFormData.push(json[0]);
+        this.dataForm = this.newFormData();
+      });
+    }
   },
   created() {
     fetch("api/members/")
