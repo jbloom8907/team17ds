d0d9aee74e1921191fcca4a603c8683a6dec8887
reabblev <reabblev@users.noreply.github.com>
2020-10-22 19:57:21 -0400

    member detailed post api

---
+++ app/public/api/memberCertifications/memberDetailed.php
@@ -0,0 +1,21 @@
+<?php
+require 'common.php';
+// Step 0: Validate the incoming data
+// This code doesn't do that, but should ...
+// For example, if the date is empty or bad, this insert fails.
+// Step 1: Get a datase connection from our helper class
+$db = DbConnection::getConnection();
+// Step 2: Create & run the query
+// Note the use of parameterized statements to avoid injection
+$stmt = $db->prepare(
+  'SELECT * FROM memberCertification WHERE CONCAT(fName, " ", lName) = ?'
+);
+$stmt->execute([
+  $_POST['selected']
+]);
+$memberCertifications = $stmt->fetchAll();
+// Step 3: Convert to JSON
+$json = json_encode($memberCertifications, JSON_PRETTY_PRINT);
+// Step 4: Output
+header('Content-Type: application/json');
+echo $json;
