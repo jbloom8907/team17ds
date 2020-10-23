23c94ef6ff21e0be0a7be7bbbcdb41fb9963a522
ash188 <70525146+ash188@users.noreply.github.com>
2020-10-22 23:14:45 -0400

    Adding certificate filtering

---
+++ app/public/tableFilter.js
@@ -0,0 +1,19 @@
+// Function found at https://www.w3schools.com/howto/howto_js_filter_table.asp
+function myFunction() {
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("expiryReport");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[1];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
