d094fbd2d44d53ed7dbf5f234bf5187c58748ce4
jbloom8907 <jbloom8907@gmail.com>
2020-11-02 12:50:45 -0500

    Final adjustments
    Adding more test data to schema,
    fixing typo

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 http://ec2-35-174-165-148.compute-1.amazonaws.com/
 
-You may use the above link to view our projects!
+You may use the above link to view our project!
 
 When updating records, be sure to fill out all input fields; this is not necessary when deleting except for the case of awarded certifications in which the certification must be filled out.
--- app/public/data/Team17_SQL.sql
+++ app/public/data/Team17_SQL.sql
@@ -17,8 +17,11 @@ CREATE TABLE member (
   radioNum VARCHAR(4) NOT NULL
 );
 
-INSERT INTO member (fName, lName, dob, gender, phonePrimary, phoneSecondary, email, street, city, state, zip, station, title, active, radioNum)
-VALUES ("Jordan", "Bloom", "1998-03-31", "M", "847-975-5699", NULL, "bloomjor@iu.edu", "508 N College Ave", "Bloomington", "Indiana", 47404, "Station 1", "Fireman", 1, "A_2");
+INSERT INTO member (fName, lName, dob, gender, phonePrimary, phoneSecondary, email, street, city, state, zip, station, title, active, radioNum) VALUES
+("Reagan", "Blevins", "1997-10-10", "F", "812-555-1234", NULL, "reabblev@iu.edu", "508 N College Ave", "Bloomington", "Indiana", 47401, "Station 1", "Captain", 1, "A_1"),
+("Jordan", "Bloom", "1998-03-31", "M", "812-555-4567", NULL, "bloomjor@iu.edu", "508 N College Ave", "Bloomington", "Indiana", 47402, "Station 2", "Sergeant", 1, "A_2"),
+("Stella", "Jiang", "1997-09-20", "F", "812-555-8910", NULL, "stjiang@iu.edu", "508 N College Ave", "Bloomington", "Indiana", 47403, "Station 3", "Fireman", 1, "A_3"),
+("Ayush", "Sarawgi", "1998-02-15", "M", "812-555-1098", NULL, "sarawgia@iu.edu", "508 N College Ave", "Bloomington", "Indiana", 47404, "Station 4", "Fireman", 1, "A_4");
 
 CREATE TABLE certification (
   certificationId INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -27,8 +30,10 @@ CREATE TABLE certification (
   standardExpiry INT NOT NULL
 );
 
-INSERT INTO certification (name, agency, standardExpiry)
-VALUES ("Sample Certification", "Red Cross", "2");
+INSERT INTO certification (name, agency, standardExpiry) VALUES
+("Ambulance Driving Certification", "Ambulance Association of America", "1"),
+("CPR Certification", "Red Cross", "2"),
+("POST Certification", "Firefighters Guild", "3");
 
 CREATE TABLE memberCertification (
   memberCertificationId INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -38,5 +43,12 @@ CREATE TABLE memberCertification (
   expiryDate DATE NOT NULL
 );
 
-INSERT INTO memberCertification (fName, lName, certName, expiryDate)
-VALUES ("Jordan", "Bloom", "Sample Certification", "2022-10-5");
+INSERT INTO memberCertification (fName, lName, certName, expiryDate) VALUES
+("Reagan", "Blevins", "Ambulance Driving Certification", "2021-11-30"),
+("Reagan", "Blevins", "CPR Certification", "2022-10-29"),
+("Jordan", "Bloom", "Ambulance Driving Certification", "2021-09-28"),
+("Jordan", "Bloom", "POST Certification", "2023-08-27"),
+("Stella", "Jiang", "CPR Certification", "2022-07-26"),
+("Stella", "Jiang", "POST Certification", "2023-06-25"),
+("Ayush", "Sarawgi", "Ambulance Driving Certification", "2018-05-24"),
+("Ayush", "Sarawgi", "POST Certification", "2019-04-23");
